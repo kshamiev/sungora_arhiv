e2d5fede0b1867073b28e406d46822a31a8fca88
Konstantin Shamiev <konstantin@shamiev.ru>
2022-03-29 14:53:08 +0300

    test update

--- lib/conf/time_test.go
+++ lib/conf/time_test.go
@@ -6,7 +6,11 @@ import (
 )
 
 func TestTime(t *testing.T) {
+	t.Log(time.Now().Format(TimeFormatGMDHIS))
 	t.Log(time.Now().Format(TimeFormatDMGHIS))
 	t.Log(time.Now().Format(TimeFormatDMG))
 	t.Log(time.Now().Format(TimeFormatHIS))
+
+	loc, _ := time.LoadLocation("Europe/Moscow")
+	t.Log(time.Now().In(loc).Format(TimeFormatGMDHIS))
 }
